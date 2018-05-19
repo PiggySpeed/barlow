221e2dcfaf5be6d3fe43edecfe0c94d98ba5498e
John Lee <jlee14c@hotmail.com>
2018-05-19 00:52:32 -0700

    completed upenn scraper

--- src/school_parsers/upenn.js
+++ src/school_parsers/upenn.js
@@ -3,22 +3,52 @@ const schools = require('../config/schools');
 const { findNodes } = require('../utils/parser_helpers');
 const { throttleRequests } = require('../utils/request_helpers');
 
+function hasLinks(elem) {
+  return elem && elem.childNodes.length > 0;
+}
+
 function areWebsites(elem) {
-  return 'Website' === elem[0].childNodes[0].value.toString().trim();
+  return elem.childNodes[0].value.toString().match(/(Website)/);
+}
+
+function extractURL(elem) {
+  // assumes elem is a valid website anchor node
+  return elem.attrs[0].value.toString().trim();
 }
 
 async function parseUPenn() {
   const html = parse5.parse(this.raw);
-  const root = schools.upenn.protocol + '//' + schools.upenn.host;
   const table = findNodes(html, 'tbody');
-  const links = findNodes(table[0], 'a', {'class': 'media-heading'});
-  const test = links.filter(areWebsites).slice(0, 5);
-  console.log(test);
-  return;
+  let rows = findNodes(table[0], 'tr');
 
-  // TODO: fix upenn
-  
-  // this.result = sites;
+  rows = rows.filter((elem) => {
+    return elem.childNodes.length === 5;
+  });
+
+  let profiles = [];
+  for (let row of rows) {
+    const nodes = row.childNodes[3].childNodes;
+    let name;
+
+    // find name
+    if (nodes[0].tagName === 'p') {
+      // some names are nested in p tags (e.g. Arvind Bhusnurmath)
+      name = nodes[0].childNodes[1].value.toString().trim();
+    } else {
+      name = nodes[1].value.toString().trim();
+    }
+
+    // find urls
+    let urls = findNodes(row, 'a');
+    urls = urls
+      .filter(hasLinks)
+      .filter(areWebsites)
+      .map(extractURL);
+
+    profiles.push({ name, urls });
+  }
+
+  this.result = profiles;
 }
 
 module.exports = parseUPenn;
